77addf3fb026ea18a4e582015a3cc2af1c53a924
Blokblokovich <blokblokovich@gmail.com>
2020-11-24 10:39:54 +0900

    iOS test

--- iOS/ChordViewer/ViewController.swift
+++ iOS/ChordViewer/ViewController.swift
@@ -7,11 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-	override func viewDidLoad() {
+class ViewController: UIViewController
+{
+	@IBOutlet weak var labelFirst: UILabel!
+	@IBOutlet weak var button: UIButton!
+	@IBOutlet weak var segmentControl: UISegmentedControl!
+	
+	override func viewDidLoad()
+	{
 		super.viewDidLoad()
+
+		button.setTitle("Push me!", for: .normal)
+		segmentControl.selectedSegmentIndex = 1
 	}
-
-
+	
+	@IBAction func pushButton(_ sender: UIButton)
+	{
+		labelFirst.text = "Hello!"
+	}
+	@IBAction func colorControl(_ sender: UISegmentedControl)
+	{
+		if sender.selectedSegmentIndex == 0
+		{
+			button.backgroundColor = .red
+		}
+		
+		else if sender.selectedSegmentIndex == 1
+		{
+			button.backgroundColor = .green
+		}
+		
+		else
+		{
+			button.backgroundColor = .blue
+		}
+	}
 }
